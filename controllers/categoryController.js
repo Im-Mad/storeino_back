be1f27a9279d9f7e1d6b7c7140ca313b0c196191
Imad.chaichaa@linux <imadchai2@gmail.com>
2022-03-18 02:24:50 +0100

    quick fix

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -28,14 +28,13 @@ exports.deleteCategory = catchAsynch(async (req, res, next) => {
     const category = await Category.findOne({ slug: req.params.category });
     if(!category) return next(new AppError("No category found",404));
     if(category.childrens.length > 0) return next(new AppError("Category has children, cannot delete",404));
+    await category.delete();
 
     if(category.level > 0) {
         const filter = { slug: category.parents[category.parents.length -1].slug };
         const update = { $pull: { childrens: { slug: category.slug, name: category.name } } };
         await Category.updateMany(filter, update);
     }
-
-    await category.delete();
 
     res.status(202).json({
         status: 'success'
