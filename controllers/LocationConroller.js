6b4ec7049cd7d9972aa462395d445776cb8afaea
Rachid El Aissaoui <rachid-tcs2000@outlook.fr>
2022-04-20 11:40:01 +0000

    get payment methods and shippers

--- controllers/LocationConroller.js
+++ controllers/LocationConroller.js
@@ -70,3 +70,87 @@ exports.getProvinces = catchAsynch(async (req, res, _next) => {
         ]
     });
 });
+
+exports.getShippers = catchAsynch(async (req, res, _next) => {
+    // const response = await Api.get('countries','search');
+    const response = {
+        results: [
+            {
+                zipCodes: [],
+                _id: "6257ecca4c52160fdc5d79bb",
+                name: "Agadir",
+                description: null,
+                shippers: [
+                    {
+                        _id: "6257ec734c52160fdc5d7952",
+                        name: "delivery guy 1",
+                        price: 0
+                    }
+                ],
+                shippingRates: [
+                    {
+                        minWeight: 0,
+                        maxWeight: 1,
+                        unite: "KG",
+                        value: 0,
+                        _id: "6257ee094c52160fdc5d7ae6"
+                    }
+                ],
+                storeId: "6202fab612f2fd0809ed2fd0",
+                __v: 0
+            },
+            {
+                zipCodes: [],
+                _id: "6257ed684c52160fdc5d7a65",
+                name: "agadir premium",
+                description: null,
+                shippers: [
+                    {
+                        _id: "6257ec8b4c52160fdc5d796f",
+                        name: "Delivery guy 2",
+                        price: 0
+                    }
+                ],
+                shippingRates: [
+                    {
+                        minWeight: 0,
+                        maxWeight: 1,
+                        unite: "KG",
+                        value: 50,
+                        _id: "6257ee204c52160fdc5d7aea"
+                    }
+                ],
+                storeId: "6202fab612f2fd0809ed2fd0",
+                __v: 0
+            }
+        ],
+        paginate: {
+            total: 2,
+            per_page: 20,
+            current_page: 1,
+            last_page: 1
+        }
+    };
+    const shippers = [];
+    response.results.forEach(item => shippers.push(...item.shippers))
+    res.status(200).json({
+        results: shippers,
+        ...response.paginate
+    });
+});
+
+exports.getMethods = catchAsynch(async  (req, res, _next) => {
+    res.status(200).json({
+        results: [
+
+            {
+                _id: "5feb408a2ef4130539efb9e0",
+                name: "CashOnDelivery"
+            },
+            {
+                _id: "5feb408a2ef4130539efb9e1",
+                name: "Paypal"
+            }
+        ]
+    })
+});
