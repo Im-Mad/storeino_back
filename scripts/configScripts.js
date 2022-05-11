32395692690cd7f72c5701c171fddbfdf7b8af0f
rachid <ra.elaissaoui@gmail.com>
2022-05-11 02:26:32 +0100

    Minor changes

--- scripts/configScripts.js
+++ scripts/configScripts.js
@@ -1,15 +1,7 @@
-const axios = require("axios");
 const dotenv = require("dotenv");
 const Config = require("../models/configModel");
 const fs = require("fs");
 const mongoose = require("mongoose");
-
-const headers = {
-    'x-auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdG9yZSI6eyJfaWQiOiI2MjAyZmFiNjEyZjJmZDA4MDllZDJmZDAifSwiaWF0IjoxNjQ3NzAxMTU2LCJleHAiOjE2NzkyMzcxNTZ9.GQGMrCDO_N57IIS2g9TUwy1DCipSRc-6oUNb1dJEx3E',
-}
-const query = "https://api-stores.storeino.com/api/products/search?limit=100";
-
-
 
 dotenv.config({ path: `${__dirname}/../config.env` });
 
@@ -50,4 +42,4 @@ const importData = async () => {
     }
 }
 
-await importData();
+importData();
